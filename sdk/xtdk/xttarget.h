21abe0553a5e93c0d04c1481b4878bd3398b7ba3
belliash <belliash@codingworkshop.eu.org>
2023-01-11 15:39:36 +0100

    Code formatting

--- sdk/xtdk/xttarget.h
+++ sdk/xtdk/xttarget.h
@@ -17,29 +17,29 @@
 
 /* Architecture specific definitions */
 #if defined(__i386__) || defined(__i686__)
-    #define _ARCH                      i686
-    #define _ARCH_I686                 1
-    #define _ARCH_NAME                 "32-bit x86"
-    #define _ARCH_IMAGE_MACHINE_TYPE   0x014C
-    #define _XT32                      1
-    #define CACHE_ALIGNMENT            64
-    #define EFI_ERROR_MASK             0x80000000
-    #define MAXIMUM_PROCESSORS         32
-    #define MEMORY_ALIGNMENT           8
-    #define MM_USERPAGE_TABLES         1536
-    #define MM_VIRTUAL_PAGESIZE        20
+    #define _ARCH                                   i686
+    #define _ARCH_I686                              1
+    #define _ARCH_NAME                              "32-bit x86"
+    #define _ARCH_IMAGE_MACHINE_TYPE                0x014C
+    #define _XT32                                   1
+    #define CACHE_ALIGNMENT                         64
+    #define EFI_ERROR_MASK                          0x80000000
+    #define MAXIMUM_PROCESSORS                      32
+    #define MEMORY_ALIGNMENT                        8
+    #define MM_USERPAGE_TABLES                      1536
+    #define MM_VIRTUAL_PAGESIZE                     20
 #elif defined(__amd64__) || defined(__x86_64__)
-    #define _ARCH                      amd64
-    #define _ARCH_AMD64                1
-    #define _ARCH_NAME                 "64-bit x86"
-    #define _ARCH_IMAGE_MACHINE_TYPE   0x8664
-    #define _XT64                      1
-    #define CACHE_ALIGNMENT            64
-    #define EFI_ERROR_MASK             0x8000000000000000
-    #define MAXIMUM_PROCESSORS         256
-    #define MEMORY_ALIGNMENT           16
-    #define MM_USERPAGE_TABLES         4194304
-    #define MM_VIRTUAL_PAGESIZE        52
+    #define _ARCH                                   amd64
+    #define _ARCH_AMD64                             1
+    #define _ARCH_NAME                              "64-bit x86"
+    #define _ARCH_IMAGE_MACHINE_TYPE                0x8664
+    #define _XT64                                   1
+    #define CACHE_ALIGNMENT                         64
+    #define EFI_ERROR_MASK                          0x8000000000000000
+    #define MAXIMUM_PROCESSORS                      256
+    #define MEMORY_ALIGNMENT                        16
+    #define MM_USERPAGE_TABLES                      4194304
+    #define MM_VIRTUAL_PAGESIZE                     52
 #else
     #error Unknown architecture
 #endif
