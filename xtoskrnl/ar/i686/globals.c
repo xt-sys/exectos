ae60753e33d4ec28474e97e91d3b145c8dbe0d5d
belliash <belliash@codingworkshop.eu.org>
2023-01-29 00:52:05 +0100

    Add missing source files for i686 architecture

--- xtoskrnl/ar/i686/globals.c
+++ xtoskrnl/ar/i686/globals.c
@@ -1,19 +1,10 @@
 /**
  * PROJECT:         ExectOS
  * COPYRIGHT:       See COPYING.md in the top level directory
- * FILE:            xtoskrnl/ar/amd64/globals.c
+ * FILE:            xtoskrnl/ar/i686/globals.c
  * DESCRIPTION:     XT architecture library global variables
  * DEVELOPERS:      Rafal Kupiec <belliash@codingworkshop.eu.org>
  */
 
 #include "xtos.h"
 
-
-/* Initial GDT */
-KGDTENTRY ArInitialGdt[GDT_ENTRIES] = {0};
-
-/* Initial IDT */
-KIDTENTRY ArInitialIdt[IDT_ENTRIES] = {0};
-
-/* Initial TSS */
-KTSS ArInitialTss;
---
+++ xtoskrnl/ar/i686/procsup.c
@@ -0,0 +1,23 @@
+/**
+ * PROJECT:         ExectOS
+ * COPYRIGHT:       See COPYING.md in the top level directory
+ * FILE:            xtoskrnl/ar/i686/procsup.c
+ * DESCRIPTION:     AMD64 processor functionality support
+ * DEVELOPERS:      Rafal Kupiec <belliash@codingworkshop.eu.org>
+ */
+
+#include "xtos.h"
+
+
+/**
+ * Initializes i686 processor specific structures.
+ *
+ * @return This routine does not return any value.
+ *
+ * @since XT 1.0
+ */
+XTAPI
+VOID
+ArInitializeProcessor(VOID)
+{
+}
