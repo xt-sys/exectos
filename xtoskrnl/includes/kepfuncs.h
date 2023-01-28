9cd9a1eafacd899d2e07c90a62b602593fb154be
belliash <belliash@codingworkshop.eu.org>
2023-01-29 00:25:25 +0100

    Correct typos

--- xtoskrnl/includes/kepfuncs.h
+++ xtoskrnl/includes/kepfuncs.h
@@ -1,13 +1,13 @@
 /**
  * PROJECT:         ExectOS
  * COPYRIGHT:       See COPYING.md in the top level directory
- * FILE:            xtoskrnl/includes/kefuncs.h
+ * FILE:            xtoskrnl/includes/kepfuncs.h
  * DESCRIPTION:     Private routine definitions for kernel services
  * DEVELOPERS:      Rafal Kupiec <belliash@codingworkshop.eu.org>
  */
 
-#ifndef __XTOSKRNL_KEFUNCS_H
-#define __XTOSKRNL_KEFUNCS_H
+#ifndef __XTOSKRNL_KEPFUNCS_H
+#define __XTOSKRNL_KEPFUNCS_H
 
 #include <xtos.h>
 
@@ -28,4 +28,4 @@ XTAPI
 VOID
 KepSwitchBootStack(IN ULONG_PTR Stack);
 
-#endif /* __XTOSKRNL_KEFUNCS_H */
+#endif /* __XTOSKRNL_KEPFUNCS_H */
