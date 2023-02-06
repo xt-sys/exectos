f8d2ccc0d0dff15c1f0d1682b0cc30ca5dc73b84
belliash <belliash@codingworkshop.eu.org>
2023-02-06 16:10:32 +0100

    Add more debugging macros

--- sdk/xtdk/xtdebug.h
+++ sdk/xtdk/xtdebug.h
@@ -10,6 +10,13 @@
 #define __XTDK_XTDEBUG_H
 
 
+/* Debugging macros */
+#define CHECKPOINT                      LdrPrint(L"Checkpoint reached at %s:%d\n", __FILE__, __LINE__);
+#define DEPRECATED                      LdrPrint(L"Called deprecated routine '%s()' at %s:%d\n", \
+                                                 __FUNCTION__, __FILE__, __LINE__);
+#define UNIMPLEMENTED                   LdrPrint(L"Called unimplemented routine '%s()' at %s:%d\n", \
+                                                 __FUNCTION__, __FILE__, __LINE__);
+
 /* XTOS platform debugging macros */
 #ifdef DBG
     #define DEBUG                       1
