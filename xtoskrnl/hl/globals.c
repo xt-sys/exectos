d8bc45e3bb2a7f3f4db0f6cd415a490bbe2d4f4c
belliash <belliash@codingworkshop.eu.org>
2023-02-13 23:41:24 +0100

    Move HL related global variables into separate file

---
+++ xtoskrnl/hl/globals.c
@@ -0,0 +1,13 @@
+/**
+ * PROJECT:         ExectOS
+ * COPYRIGHT:       See COPYING.md in the top level directory
+ * FILE:            xtoskrnl/hl/globals.c
+ * DESCRIPTION:     Architecture independent global variables related to HL subsystem
+ * DEVELOPERS:      Rafal Kupiec <belliash@codingworkshop.eu.org>
+ */
+
+#include <xtos.h>
+
+
+/* FrameBuffer information */
+HAL_FRAMEBUFFER_DATA HlpFrameBufferData;
--- xtoskrnl/ke/globals.c
+++ xtoskrnl/ke/globals.c
@@ -2,15 +2,12 @@
  * PROJECT:         ExectOS
  * COPYRIGHT:       See COPYING.md in the top level directory
  * FILE:            xtoskrnl/ke/globals.c
- * DESCRIPTION:     XT kernel global variables
+ * DESCRIPTION:     Architecture independent global variables related to KE subsystem
  * DEVELOPERS:      Rafal Kupiec <belliash@codingworkshop.eu.org>
  */
 
 #include <xtos.h>
 
-
-/* FrameBuffer information */
-HAL_FRAMEBUFFER_DATA HlpFrameBufferData;
 
 /* Pointer to boot loader provided DbgPrint() routine */
 VOID (*KeDbgPrint)(IN PWCHAR Format, IN ...) = NULL;
