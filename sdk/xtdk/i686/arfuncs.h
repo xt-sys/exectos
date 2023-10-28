ad15c55a399e198f507d66f8db30c19169a4a6a8
belliash <belliash@codingworkshop.eu.org>
2023-10-28 23:35:34 +0200

    Cleanup AR subsystem headers

--- sdk/xtdk/i686/arfuncs.h
+++ sdk/xtdk/i686/arfuncs.h
@@ -15,101 +15,18 @@
 #include <i686/xtstruct.h>
 
 
-/* Architecture library routines forward references */
+/* Routines used by XTLDR */
 XTCDECL
 VOID
 ArClearInterruptFlag();
 
 XTCDECL
-BOOLEAN
-ArCpuId(IN OUT PCPUID_REGISTERS Registers);
-
-XTCDECL
 VOID
 ArHalt();
-
-XTAPI
-VOID
-ArInitializeProcessor(VOID);
-
-XTCDECL
-VOID
-ArInvalidateTlbEntry(IN PVOID Address);
-
-XTCDECL
-VOID
-ArLoadGlobalDescriptorTable(IN PVOID Source);
-
-XTCDECL
-VOID
-ArLoadInterruptDescriptorTable(IN PVOID Source);
-
-XTCDECL
-VOID
-ArLoadLocalDescriptorTable(IN USHORT Source);
-
-XTCDECL
-VOID
-ArLoadSegment(IN USHORT Segment,
-              IN ULONG Source);
-
-XTCDECL
-VOID
-ArLoadTaskRegister(USHORT Source);
-
-XTCDECL
-ULONG_PTR
-ArReadControlRegister(IN USHORT ControlRegister);
-
-XTCDECL
-ULONG
-ArReadFSDualWord(ULONG Offset);
-
-XTCDECL
-ULONGLONG
-ArReadModelSpecificRegister(IN ULONG Register);
-
-XTCDECL
-ULONGLONG
-ArReadTimeStampCounter();
-
-XTCDECL
-VOID
-ArSetInterruptFlag();
-
-XTCDECL
-VOID
-ArStoreGlobalDescriptorTable(OUT PVOID Destination);
-
-XTCDECL
-VOID
-ArStoreInterruptDescriptorTable(OUT PVOID Destination);
-
-XTCDECL
-VOID
-ArStoreSegment(IN USHORT Segment,
-               OUT PVOID Destination);
-
-XTCDECL
-VOID
-ArStoreTaskRegister(OUT PVOID Destination);
 
 XTCDECL
 VOID
 ArWriteControlRegister(IN USHORT ControlRegister,
                        IN UINT_PTR Value);
-
-XTCDECL
-VOID
-ArWriteEflagsRegister(IN UINT_PTR Value);
-
-XTCDECL
-VOID
-ArWriteModelSpecificRegister(IN ULONG Register,
-                             IN ULONGLONG Value);
-
-XTCDECL
-VOID
-ArYieldProcessor();
 
 #endif /* __XTDK_I686_ARFUNCS_H */
--- xtoskrnl/includes/i686/ar.h
+++ xtoskrnl/includes/i686/ar.h
@@ -12,11 +12,107 @@
 #include <xtos.h>
 
 
+XTCDECL
+VOID
+ArClearInterruptFlag();
+
+XTCDECL
+BOOLEAN
+ArCpuId(IN OUT PCPUID_REGISTERS Registers);
+
+XTCDECL
+VOID
+ArHalt();
+
+XTAPI
+VOID
+ArInitializeProcessor(VOID);
+
+XTCDECL
+VOID
+ArInvalidateTlbEntry(IN PVOID Address);
+
+XTCDECL
+VOID
+ArLoadGlobalDescriptorTable(IN PVOID Source);
+
+XTCDECL
+VOID
+ArLoadInterruptDescriptorTable(IN PVOID Source);
+
+XTCDECL
+VOID
+ArLoadLocalDescriptorTable(IN USHORT Source);
+
+XTCDECL
+VOID
+ArLoadSegment(IN USHORT Segment,
+              IN ULONG Source);
+
+XTCDECL
+VOID
+ArLoadTaskRegister(USHORT Source);
+
+XTCDECL
+ULONG_PTR
+ArReadControlRegister(IN USHORT ControlRegister);
+
+XTCDECL
+ULONG
+ArReadFSDualWord(ULONG Offset);
+
+XTCDECL
+ULONGLONG
+ArReadModelSpecificRegister(IN ULONG Register);
+
+XTCDECL
+ULONGLONG
+ArReadTimeStampCounter();
+
 XTAPI
 VOID
 ArSetGdtEntryBase(IN PKGDTENTRY Gdt,
                   IN USHORT Selector,
                   IN ULONG_PTR Base);
+
+XTCDECL
+VOID
+ArSetInterruptFlag();
+
+XTCDECL
+VOID
+ArStoreGlobalDescriptorTable(OUT PVOID Destination);
+
+XTCDECL
+VOID
+ArStoreInterruptDescriptorTable(OUT PVOID Destination);
+
+XTCDECL
+VOID
+ArStoreSegment(IN USHORT Segment,
+               OUT PVOID Destination);
+
+XTCDECL
+VOID
+ArStoreTaskRegister(OUT PVOID Destination);
+
+XTCDECL
+VOID
+ArWriteControlRegister(IN USHORT ControlRegister,
+                       IN UINT_PTR Value);
+
+XTCDECL
+VOID
+ArWriteEflagsRegister(IN UINT_PTR Value);
+
+XTCDECL
+VOID
+ArWriteModelSpecificRegister(IN ULONG Register,
+                             IN ULONGLONG Value);
+
+XTCDECL
+VOID
+ArYieldProcessor();
 
 XTCDECL
 VOID
