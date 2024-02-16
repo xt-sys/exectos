70c66a5d706754dfd894e2012a11d565caa93da2
Rafal Kupiec <belliash@codingworkshop.eu.org>
2024-02-16 22:44:19 +0100

    Code formatting

--- xtldr/arch/i686/memory.c
+++ xtldr/arch/i686/memory.c
@@ -125,8 +125,8 @@ BlBuildPageMap(IN PXTBL_PAGE_MAPPING PageMap)
         if(Mapping->VirtualAddress)
         {
             /* Dump memory mapping */
-            BlDebugPrint(L"   Type=%02lu, PhysicalBase=%.8p, VirtualBase=%.8p, Pages=%lu\n",
-                         Mapping->MemoryType, Mapping->PhysicalAddress, Mapping->VirtualAddress, Mapping->NumberOfPages);
+            BlDebugPrint(L"   Type=%02lu, PhysicalBase=%.8P, VirtualBase=%.8P, Pages=%lu\n", Mapping->MemoryType,
+                         Mapping->PhysicalAddress, Mapping->VirtualAddress, Mapping->NumberOfPages);
 
             /* Map memory */
             Status = BlMapPage(PageMap, (UINT_PTR)Mapping->VirtualAddress,
