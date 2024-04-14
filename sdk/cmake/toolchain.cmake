ffda2fd8a9e547abcbafbd18afbf8bd8a9cb7186
Rafal Kupiec <belliash@codingworkshop.eu.org>
2024-04-14 11:18:31 +0200

    Update toolchain settings

--- sdk/cmake/toolchain.cmake
+++ sdk/cmake/toolchain.cmake
@@ -2,17 +2,23 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
 # Set toolchain compilers
-set(CMAKE_ASM_COMPILER llvm-ml)
+set(CMAKE_ASM_COMPILER clang-cl)
 set(CMAKE_C_COMPILER clang-cl)
 set(CMAKE_CXX_COMPILER clang-cl)
 set(CMAKE_MC_COMPILER wmc)
 set(CMAKE_RC_COMPILER wrc)
 set(CMAKE_SPEC_COMPILER xtcspecc)
 
-# Assume that C/C++ compiler is working
+# Assume that ASM and C/C++ compilers are working
 set(CMAKE_ASM_COMPILER_WORKS 1)
 set(CMAKE_C_COMPILER_WORKS 1)
 set(CMAKE_CXX_COMPILER_WORKS 1)
+
+# Set C/C++ standard and disable extensions
+set(CMAKE_C_EXTENSIONS OFF)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_C_STANDARD 23)
+set(CMAKE_CXX_STANDARD 23)
 
 # Disable standard C libraries
 set(CMAKE_C_STANDARD_LIBRARIES "" CACHE INTERNAL "")
