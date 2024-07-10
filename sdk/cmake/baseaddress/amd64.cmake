94076b7471e60ed2e289a82b54edb350fbea5b53
Rafal Kupiec <belliash@codingworkshop.eu.org>
2024-07-10 22:42:48 +0200

    Separate image base address per architecture

---
+++ sdk/cmake/baseaddress/amd64.cmake
@@ -0,0 +1,2 @@
+# Set base addresses for all modules
+set(BASEADDRESS_XTOSKRNL 0x0000000140000000)
--- sdk/cmake/baseaddress/i686.cmake
+++ sdk/cmake/baseaddress/i686.cmake
@@ -1,3 +1,2 @@
 # Set base addresses for all modules
-set(BASEADDRESS_XTLDR    0x00010000)
 set(BASEADDRESS_XTOSKRNL 0x00400000)
