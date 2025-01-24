e0125dda54ed341f30fe3fae7bc624c24b9e87c9
Aiken Harris <harraiken@noreply.codingworkshop.git>
2025-01-24 09:19:59 +0100

    Use virtual FAT disk images

--- sdk/cmake/emulation.cmake
+++ sdk/cmake/emulation.cmake
@@ -27,22 +27,22 @@ add_custom_target(bochsvm
 
 # This target starts up a QEMU+OVMF virtual machine using KVM accelerator
 add_custom_target(testkvm
-                  DEPENDS diskimg
+                  DEPENDS install
                   COMMAND ${QEMU_COMMAND} -name "ExectOS-${ARCH}-KVM" -machine type=q35,kernel_irqchip=on,accel=kvm,mem-merge=off,vmport=off -enable-kvm -cpu host,-hypervisor,+topoext
                                           -smp 2,sockets=1,cores=1,threads=2 -m 4G -overcommit mem-lock=off -rtc clock=host,base=localtime,driftfix=none
                                           -drive file=${EXECTOS_SOURCE_DIR}/sdk/firmware/ovmf_code_${ARCH}.fd,if=pflash,format=raw,unit=0,readonly=on
                                           -drive file=${EXECTOS_SOURCE_DIR}/sdk/firmware/ovmf_vars_${ARCH}.fd,if=pflash,format=raw,unit=1
-                                          -hda ${EXECTOS_BINARY_DIR}/output/disk.img
+                                          -hda fat:rw:${EXECTOS_BINARY_DIR}/output/binaries
                                           -boot menu=on -d int -M smm=off -no-reboot -no-shutdown -serial stdio
                   VERBATIM USES_TERMINAL)
 
 # This target starts up a QEMU+OVMF virtual machine using TCG accelerator
 add_custom_target(testtcg
-                  DEPENDS diskimg
+                  DEPENDS install
                   COMMAND ${QEMU_COMMAND} -name "ExectOS-${ARCH}-TCG" -machine type=q35,accel=tcg -cpu max,-hypervisor
                                           -smp 2,sockets=1,cores=1,threads=2 -m 4G -overcommit mem-lock=off -rtc clock=host,base=localtime,driftfix=none
                                           -drive file=${EXECTOS_SOURCE_DIR}/sdk/firmware/ovmf_code_${ARCH}.fd,if=pflash,format=raw,unit=0,readonly=on
                                           -drive file=${EXECTOS_SOURCE_DIR}/sdk/firmware/ovmf_vars_${ARCH}.fd,if=pflash,format=raw,unit=1
-                                          -hda ${EXECTOS_BINARY_DIR}/output/disk.img
+                                          -hda fat:rw:${EXECTOS_BINARY_DIR}/output/binaries
                                           -boot menu=on -d int -M smm=off -no-reboot -no-shutdown -serial stdio
                   VERBATIM USES_TERMINAL)
