3e097c260dd52b8c0842df769d93d09e093bbc8f
Aiken Harris <harraiken91@gmail.com>
2025-09-15 20:52:40 +0200

    Add stubs for C++ exception and purecall handlers

--- xtoskrnl/rtl/amd64/exsup.cc
+++ xtoskrnl/rtl/amd64/exsup.cc
@@ -4,6 +4,7 @@
  * FILE:            xtoskrnl/rtl/amd64/exsup.cc
  * DESCRIPTION:     Exception handling for AMD64 architecture
  * DEVELOPERS:      Rafal Kupiec <belliash@codingworkshop.eu.org>
+ *                  Aiken Harris <harraiken91@gmail.com>
  */
 
 #include <xtos.hh>
@@ -25,6 +26,24 @@ __C_specific_handler(IN PEXCEPTION_RECORD ExceptionRecord,
 
 XTCLINK
 XTCDECL
+EXCEPTION_DISPOSITION
+ __CxxFrameHandler3(IN PEXCEPTION_RECORD ExceptionRecord,
+                     IN PVOID EstablisherFrame,
+                     IN OUT PCONTEXT ContextRecord,
+                     IN OUT PVOID DispatcherContext)
+{
+    UNIMPLEMENTED;
+
+    /* Disable interrupts and hang */
+    AR::CpuFunc::ClearInterruptFlag();
+    KE::Crash::Panic(0); // CXX_FRAME_HANDLER_CALLED
+
+    /* Continue search */
+    return ExceptionContinueSearch;
+}
+
+XTCLINK
+XTCDECL
 INT
 _except_handler3(PEXCEPTION_RECORD ExceptionRecord,
                  PVOID Registration,
@@ -35,4 +54,16 @@ _except_handler3(PEXCEPTION_RECORD ExceptionRecord,
 
     /* Handler not found */
     return 0;
+}
+
+XTCLINK
+XTCDECL
+VOID
+_purecall(VOID)
+{
+    UNIMPLEMENTED;
+
+    /* Disable interrupts and hang */
+    AR::CpuFunc::ClearInterruptFlag();
+    KE::Crash::Panic(0); // PURE_VIRTUAL_FUNCTION_CALL
 }
