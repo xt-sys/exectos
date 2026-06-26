77c1138f7da6b21c0e8d388852e5347a6226cd3c
Aiken Harris <harraiken91@gmail.com>
2026-06-26 09:54:38 +0200

    Add CheckApcDelivery to evaluate APC interrupt request or direct delivery

--- xtoskrnl/includes/ke/apc.hh
+++ xtoskrnl/includes/ke/apc.hh
@@ -18,6 +18,10 @@ namespace KE
     class Apc
     {
         public:
+            STATIC XTAPI VOID CheckApcDelivery(VOID);
+            STATIC XTAPI VOID DeliverApc(IN KPROCESSOR_MODE ProcessorMode,
+                                         IN PKEXCEPTION_FRAME ExceptionFrame,
+                                         IN PKTRAP_FRAME TrapFrame);
             STATIC XTAPI VOID InitializeApc(IN PKAPC Apc,
                                             IN PKTHREAD Thread,
                                             IN KAPC_ENVIRONMENT Environment,
