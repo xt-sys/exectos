279aba9742a7d6145d995be5829c4363be639ac1
Aiken Harris <harraiken91@gmail.com>
2026-07-08 23:25:31 +0200

    Add process quota block pool tag

--- xtoskrnl/includes/mm/tags.hh
+++ xtoskrnl/includes/mm/tags.hh
@@ -19,6 +19,7 @@
 #define TAG_MM_OVERFLOW                                         SIGNATURE32('O', 'V', 'F', 'L')
 #define TAG_MM_MEMORY_POOL                                      SIGNATURE32('P', 'O', 'O', 'L')
 #define TAG_MM_NONE                                             SIGNATURE32('N', 'O', 'N', 'E')
+#define TAG_PS_QUOTA_BLOCK                                      SIGNATURE32('P', 'S', 'Q', 'B')
 #define TAG_SE_DESCRIPTOR                                       SIGNATURE32('S', 'E', 'S', 'D')
 
 #endif /* __XTOSKRNL_MM_TAGS_HH */
